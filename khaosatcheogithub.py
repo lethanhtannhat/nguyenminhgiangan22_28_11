37f4c82fce48468041d953337e0a172938edf539
lethanhtannhat <146319062+lethanhtannhat@users.noreply.github.com>
2025-12-13 22:10:02 +0700

    Update khaosatcheogithub.py

--- khaosatcheogithub.py
+++ khaosatcheogithub.py
@@ -264,11 +264,12 @@ time.sleep(0.6)
 submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div[2]')))
 driver.execute_script("arguments[0].click();", submit_button)
 time.sleep(0.6)
-time.sleep(random.randint(60,180))
+time.sleep(random.randint(60,100))
 with open("index.txt", "w") as f:
     f.write(str(index + 1))
 time.sleep(10)     
 driver.close()
+
 
 
 
